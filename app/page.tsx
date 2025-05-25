02e70ea098122ffd1df55f8dddacd3225aa8a959
Sahand Ghavidel <sahand.gha@gmail.com>
2025-05-25 17:46:19 +0700

    Add About and Contact pages, implement Footer and Guest components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-export default function HomePage() {
+import Guest from '@/components/Guest';
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4">Sleep Tracker</h1>
-      <div className="text-red-600">HomePage</div>
+      <h1 className='text-3xl font-bold mb-4'>Sleep Tracker</h1>
+      <div className='text-red-600'>HomePage</div>
     </div>
   );
 }
